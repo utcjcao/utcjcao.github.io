a41698339f141bdc4e7aa35fca2bac5565d279d4
utcjcao <157972181+utcjcao@users.noreply.github.com>
2024-08-10 02:14:50 -0400

    onemore try

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+//<img src = "./IMG_1381.JPG"/>
 function App() {
   return (
     <div>
       <p>hi, my name is chris</p>
-      <img src = "./IMG_1381.JPG"/>
+      
     </div>
   );
 }
